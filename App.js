23a01d7f5c94bf462692b72ff166d1c46c278a66
B00152372 Aleena Gillani <B00152372@mytudublin.ie>
2024-09-23 16:37:46 +0100

    git first app working

---
+++ App.js
@@ -0,0 +1,19 @@
+import {View,Text} from 'react-native';
+
+export default function App (){
+  return(
+    <View>
+      <Text>Hello, world </Text>
+    </View>
+  ) 
+};
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default function App
